d3f1c1d3b5e1c87c02e961dc8b4577b234238c67
krishna shrivastava <krishnashrivastava040706@gmail.com>
2026-09-07 21:19:10 +0530

    modify A.java

--- 6_cycle_sort/A.java
+++ 6_cycle_sort/A.java
@@ -9,5 +9,16 @@ class A {
         System.out.println("Unsorted Array = " + Arrays.toString(x));
     }
 
-    
+    static void cycleSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] - 1 == i) {
+                i++;
+            } else {
+                int temp = arr[arr[i] - 1];
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    } 
 }
